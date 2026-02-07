235176abcfa3cfc3161263f7e9726ecf2dac5483
WillyxWonka <kalif.edu@gmail.com>
2026-02-06 17:51:09 -0800

    finished pt. 4 of assignment

--- public/js/contactform.js
+++ public/js/contactform.js
@@ -1,46 +1,47 @@
 
 const doc =document.getElementById("contact-form");
 
-doc.onsubmit = () => {
+let fname =     document.getElementById("f-name").value.trim();
+let lname =     document.getElementById("l-name").value.trim();
+let otherMet =  document.getElementById("other-met").value.trim();
+let email =     document.getElementById("email").value.trim();
+let linkedIn =  document.getElementById("linkedin").value.trim();
+let met =       document.getElementById("met-select");
+let radioHtml = document.getElementById("html");
+let radioText = document.getElementById("text");
+let MailListToggle = document.getElementById("mail-list");
+let formatOptions = document.getElementById("format-div");
+
+
+ doc.onsubmit = () => {
     let isValid = true;
     ResetErrors();
-
+    
     let invalidFields = [] // this is populated with the string id in the html of the error span indicator
-    let inputs = {}; // this is a kvp object containing input values from the html
-    
-    let fname =     document.getElementById("f-name").value.trim();
-    let lname =     document.getElementById("l-name").value.trim();
-    let job =       document.getElementById("job").value.trim();
-    let company =   document.getElementById("company").value.trim();
-    let linkedIn =  document.getElementById("linkedin").value.trim();
-    let email =     document.getElementById("email").value.trim();
-    let otherMet =  document.getElementById("other-met").value.trim();
-    let met =       document.getElementById("met-select");
-    let radioHtml = document.getElementById("html");
-    let radioText = document.getElementById("text");
-    
-    inputs['err-fname'] =   fname;
-    inputs['err-lname'] =   lname;
-    inputs['err-job'] =     job;
-    inputs['err-company']=  company;
-    inputs['err-linkedIn']= linkedIn;
-    inputs['err-email'] =   email;
 
-    //if and of the input values are invalid this loop adds the key to the invalid list
-    for(var key of Object.keys(inputs)){
-        if(!inputs[key]){
-            invalidFields.push(key)
-        }
-    }
+    if(!fname){invalidFields.push("err-fname");}
+    if(!lname){invalidFields.push("err-lname");}
 
     //this if checks if there is a value for wither the other input or the select
     if(met.value == "none" && !otherMet ){
         invalidFields.push("err-met");
         invalidFields.push("err-met-other");
     }
-    // verifies if a format radio was selected
-    if(!radioHtml.checked && !radioText.checked){
-        invalidFields.push("err-format");
+    //reuires email if add to mailing list is toggled
+    if(MailListToggle.checked){
+        if(!email){
+            invalidFields.push("err-email");
+        }
+        // verifies if a format radio was selected
+        if(!radioHtml.checked && !radioText.checked){
+            invalidFields.push("err-format");
+        }
+    }
+    // validates linked in format if one is provided
+    if(linkedIn){
+        if(!linkedIn.includes("https://linkedin.com/in/")){
+            invalidFields.push("err-linkedIn");
+        }
     }
     
 
@@ -53,9 +54,9 @@ doc.onsubmit = () => {
             document.getElementById(errID).style.display = 'block';
         }
     }
-
     return isValid;
 }
+
 
 //reset all error displays to none
 function ResetErrors(){
@@ -64,3 +65,18 @@ function ResetErrors(){
         e.style.display='none';
     }
 }
+
+// adds a listener to email check that toggles visability of format options toggles.
+// resets format toggles, and email errror display, if mailing list is toggled off of 
+MailListToggle.addEventListener('change', (e) => {
+    if(e.target.checked) {
+        formatOptions.style.display = "flex";
+    } 
+    else {
+        formatOptions.style.display = "none";
+        document.getElementById('err-format').style.display = 'none';
+        document.getElementById("err-email").style.display = 'none';
+        radioHtml.checked = false
+        radioText.checked = false
+    }
+});
